c95836adba6e06c640824ecf8936c6e918c0cb32
MiqueasLH <miqueaslaynas@gmail.com>
2015-05-28 23:40:28 -0700

    commented da codeses

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var express = require("express"),
   session = require("express-session"),
   app = express(),
   path = require("path"),
-  views = path.join(__dirname, "views");
+  views = path.join(__dirname, "views"),
+  request = require("request");
 
 //middleware for express
 var loginHelpers = function(req, res, next) {
@@ -50,34 +51,39 @@ app.use(session({
 }));
 
 //get requests to the server sending files back (htmlpages)
+//serves up landing page
 app.get("/", function(req, res) {
+  // defines homePath
   var homePath = path.join(views, "login.html");
+  //send home page
   res.sendFile(homePath);
 });
-
+//servers up story html page
 app.get("/story", function(req, res) {
   var storyPath = path.join(views, "story.html");
   res.sendFile(storyPath);
 });
-
+//serves up login page
 app.get("/login", function(req, res) {
   var loginPath = path.join(views, "login.html");
 
   res.sendFile(loginPath);
 });
-
+//gets story from DB
 app.get("/stories", function (req, res) {
+  //look for everythign in story (we only have one thing in there)
   db.Story.find({}, function (err, results) {
     console.log(results);
+    //convert results to JSON string
     res.send(JSON.stringify(results));
   });
 });
-
+//server the regitration page
 app.get("/register", function(req, res) {
   var registerPath = path.join(views, "register.html");
   res.sendFile(registerPath);
 });
-
+//server profile page
 app.get("/profile", function(req, res) {
   req.currentUser(function(err, user) {
     if (user) {
@@ -88,13 +94,37 @@ app.get("/profile", function(req, res) {
     }
   });
 });
-
+//calls logout function from loginhelper middleware and redirects to landing
 app.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
 });
-//post requests to server
+//api req to giphy does nothing atm
+app.get('/top_gif', function(req, res){
 
+  console.log("Requesting data from giphy");
+  request.get({
+    uri: "http://api.giphy.com/v1/gifs/trending",
+    qs: {
+      limit: 1,
+      api_key: "dc6zaTOxFJmzC"
+    }
+  }, function(err, apiRes, apiBody){
+
+      if (err) {
+        console.log("error for Giphy.");
+        res.send("There was an error");
+      }
+
+      console.log("Giphy api res");
+      var body = JSON.parse(apiRes.body);
+
+      console.log("Grabbing the top trending gif");
+      res.send("<img src='" + body.data[0].images.original.url + "'>");
+
+  });
+
+});
 //login request to server takes you to story on success
 app.post("/login", function(req, res) {
   var user = req.body.user;
@@ -102,10 +132,12 @@ app.post("/login", function(req, res) {
   db.User.
   authenticate(user, function(err, user) {
     console.log(user);
+    //if you exist proceed to story
     if (user) {
       console.log("LOGGING IN");
       req.login(user);
       res.redirect("/story");
+      //else get made son
     } else {
       console.log("failed to login");
       res.redirect("/login");
@@ -126,20 +158,19 @@ app.post("/user", function(req, res) {
     }
   });
 });
-//need route to update story
-// app.post("/story", function(req, res) {
-//   var
-// })
+//postint to story database this was a pain... THE PAIN!!!!
 app.post("/story", function (req, res) {
   console.log(req.body);
   var newStory = req.body.data;
+  //finally got the data
   console.log("received newStory", newStory);
+  //update the story.words with sent data
   db.Story.findByIdAndUpdate({_id:"55675ef128d1c9b010993296"}, {words: newStory}, {new:true}, function (err, story) {
     console.log(story);
     res.send(story);
   });
 });
-
+//heroku sorcery!
 app.listen(process.env.PORT || 3000, function() {
   console.log("Running!");
 });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
 function View() {}
 
-
+//render story function
 View.render = function(item) {
 var story = item[0].words;
 $("#storyWords").empty().append(story);
@@ -23,26 +23,31 @@ Story.all = function() {
     // remove the quotes from words:
     var story = JSON.parse(res);
     console.log(story);
-    // render the
+    // render the story call back
     View.render(story);
   });
 };
-
+//update the story request
 Story.update = function () {
   $("#update-form").on("submit", function (e) {
 
     e.preventDefault();
     console.log("form submited");
-
+    //take current story content
     var existingStory = $("#storyWords").text();
     console.log(existingStory);
+    //get user story input
     var storyParams = $(this.words).val();
     console.log(storyParams);
+    //add story input to end of existingStory
     var newStory = existingStory + storyParams;
     console.log(newStory);
+    //send it to story
     $.post("/story", {data: newStory}, function (res) {
       console.log(res);
+      //reset the text area
       $("#update-form")[0].reset();
+      //call story.all
       Story.all();
     });
 
